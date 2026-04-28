f28c2de034d1cd6b804c109e53b9bf5e68aab6e7
Pablo Lopez <pablo.lopez02@csioviedo.com>
2026-04-28 11:36:16 +0200

    fix digest

--- addons/digest/models/digest.py
+++ addons/digest/models/digest.py
@@ -155,6 +155,8 @@ class Digest(models.Model):
             if digest in to_slowdown:
                 digest.periodicity = digest._get_next_periodicity()[0]
             digest.next_run_date = digest._get_next_run_date()
+            
+            
 
     def _action_send_to_user(self, user, tips_count=1, consume_tips=True):
         unsubscribe_token = self._get_unsubscribe_token(user.id)
