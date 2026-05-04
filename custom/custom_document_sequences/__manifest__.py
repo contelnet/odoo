1d405b8cb8b32f03d6647cca4ad47d3ea1ba3048
Pablo Lopez <pablo.lopez02@csioviedo.com>
2026-05-04 11:31:36 +0200

    secuencias fctura

---
+++ custom/custom_document_sequences/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Secuencias personalizadas para Albarán y Presupuesto",
+    "version": "16.0.1.0.0",
+    "category": "Custom",
+    "summary": "Nomenclatura ALB para albarán y PPT para presupuesto.",
+    "author": "contelnet, GitHub Copilot",
+    "depends": ["stock", "sale_management"],
+    "data": [
+        "data/sequences.xml"
+    ],
+    "installable": True,
+    "application": False,
+}
