da1bd62c10de022333e26ea318222bd3f6e4d859
gstoicacontelnet <g.stoica@contelnet.com>
2026-09-09 11:49:31 +0200

    forzar actualizacion

--- addons/purchase_manual_paid/models/helpdesk_ticket.py
+++ addons/purchase_manual_paid/models/helpdesk_ticket.py
@@ -3,5 +3,5 @@ from odoo import models, fields
 class HelpdeskTicket(models.Model):
     _inherit = "helpdesk.ticket"
 
-    # Pisamos el campo original para meterle el HTML
+    # Pisamos el campo original para meterle el HTML.
     description = fields.Html(string="Descripción", sanitize=True)
